743e03439f5fffe785964450f0bb59b252356486
SohailSajid <sohailsajid79@gmail.com>
2024-10-22 23:05:42 +0100

    upload ecr module

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "tm_app" {
-  name                 = "tm-app"
+  name                 = var.ecr_repository_name
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
